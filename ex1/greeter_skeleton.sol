e176b204c58cd77a45a99f30d9141e06d064c95f
Nishaad Navkal <nknavkal@berkeley.edu>
2017-09-15 10:53:27 -0700

    I'm not sure I did this right at all, I don't really get how to test this code

--- ex1/greeter_skeleton.sol
+++ ex1/greeter_skeleton.sol
@@ -3,13 +3,13 @@ pragma solidity ^0.4.13;
 
 contract greeter {
 	/* Add one variable to hold our greeting */
-	string greeting;
+	string greeting = "Hello world!";"
 
-	function greeter(string _greeting) public {
-		/* Write one line of code for the contract to set our greeting */
-	}
+	}function greeter(string greeting) public {
+        greeting = "Hello world!"
+    }
 
-	function greet() constant returns (string)  {
-		/* Write one line of code to allow the contract to return our greeting */
+	function greet() constant returns (string greetaroni)  {
+		return greeting();
 	}
 }
--- ex2/fibonacci_skeleton.sol
+++ ex2/fibonacci_skeleton.sol
@@ -3,8 +3,11 @@ pragma solidity ^0.4.16;
 
 contract Fibonacci {
 	function calculate(uint position) returns (uint result) {
-		/* Add one variable to hold our greeting */
-	}
+        if (position < 2) return position;
+        else
+            return calculate(position - 1) + calculate(position - 2);
+    }
 
-	/* Add a fallback function to prevent contract payability and non-existent function calls */
-}
+	function() {
+	/// fallback does nothing?
+	}
